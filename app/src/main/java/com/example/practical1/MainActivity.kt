18f4fe577bb1bb50dc3c7f307da6b56bfb020367
abkec <davidkor1999@gmail.com>
2019-10-17 21:32:07 +0800

    commit

--- app/src/main/java/com/example/practical1/MainActivity.kt
+++ app/src/main/java/com/example/practical1/MainActivity.kt
@@ -15,15 +15,44 @@ class MainActivity : AppCompatActivity() {
 
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener { rollDice() }
+
+        val countUpButton: Button = findViewById(R.id.countUp_button)
+        countUpButton.setOnClickListener { countUp() }
     }
 
     private fun rollDice() {
-        //Toast.makeText(this, "button clicked",
-          //  Toast.LENGTH_SHORT).show()
+
         val resultText: TextView = findViewById(R.id.result_text)
-        //resultText.text = "Dice Rolled!"
         val randomInt = Random().nextInt(6) + 1
         resultText.text = randomInt.toString()
 
+        val resultText2: TextView = findViewById(R.id.result_text2)
+        val randomInt2 = Random().nextInt(6) + 1
+        resultText2.text = randomInt2.toString()
+
+        val resultText3: TextView = findViewById(R.id.result_text3)
+        val randomInt3 = Random().nextInt(6) + 1
+        resultText3.text = randomInt3.toString()
+    }
+
+    private fun countUp() {
+
+        val resultText: TextView = findViewById(R.id.result_text)
+
+        if (resultText.text == "Hello World!") {
+
+            resultText.text = "1"
+        }
+
+        else {
+
+            var resultInt = resultText.text.toString().toInt()
+
+            if (resultInt < 6) {
+
+                resultInt++
+                resultText.text = resultInt.toString()
+            }
+        }
     }
 }
